a5b28341afbb54c5126d5f6c50a87d559a4eaa60
Chloe Jouin <chloecassanjouin@gmail.com>
2024-05-29 15:25:36 +0200

    Lodging

--- src/pages/Lodging.jsx
+++ src/pages/Lodging.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from "../components/Header";
+import Footer from '../components/Footer';
 
 export default function Lodging() {
   return (
     <>
       <Header />
       <h1>Lodging page</h1>
+      <Footer />
     </>
   );
 }
