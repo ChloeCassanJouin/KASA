fa422e5cc6a7fe7078a33ac55bae1092c323465f
Chloe Jouin <chloecassanjouin@gmail.com>
2024-06-06 18:35:08 +0200

    structure SASS

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -9,7 +9,7 @@ export default function NoPage() {
       <div className='errorContainer'>
         <p className='errorNumber'>404</p>
         <p className='errorText1'>Oups! La page que <span class="break"> vous demandez n'existe pas.</span></p>
-        <a href="/home" className='errorText2'>Retourner sur la page d'accueil</a>
+        <a href="/" className='errorText2'>Retourner sur la page d'accueil</a>
       </div>
       <Footer />
     </>
